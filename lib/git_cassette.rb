e53b9db4c55346d216f31e80b6fa79dd50d108ef
Caleb Buxton <me@cpb.ca>
2025-05-17 13:29:57 -0700

    🤡 fake implementation

--- lib/git_cassette.rb
+++ lib/git_cassette.rb
@@ -1,7 +1,12 @@
 require "ostruct"
+require "fileutils"
 
 module GitCassette
   def self.record(name, &block)
+    FileUtils.mkdir_p('cassettes')
+    FileUtils.mkdir_p('repos')
+    FileUtils.touch("cassettes/#{name}.bundle")
+    FileUtils.mkdir_p("repos/#{name}")
     block.call
   end
 
