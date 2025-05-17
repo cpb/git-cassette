f09315bee1f61943035a1b2e2c0355e4d5e59d96
cpb <cpb@example.com>
2025-05-17 12:26:27 -0700

    ♻️ chore(cucumber): remove implementation references from env.rb for pure acceptance testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'aruba/cucumber'
 require 'rspec/expectations'
-require 'git_cassette'
 
 Aruba.configure do |config|
   config.command_launcher = :in_process
-  config.main_class = GitCassette::CLI
 end 
